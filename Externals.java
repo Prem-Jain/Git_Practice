6c2db06870107bd2440de83e5bcfabdd70f149a0
Prem-Jain <premj801@gmail.com>
2022-06-30 13:25:01 +0530

    EXTERNALS EXAM

---
+++ Externals.java
@@ -0,0 +1,7 @@
+class Externals
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello. This is externals examination. I have got 5th question. That is UML diagrams for Banking System and GIT version Control. Todays Date is 30 June. The time is 1:24pm.");
+	}
+}
